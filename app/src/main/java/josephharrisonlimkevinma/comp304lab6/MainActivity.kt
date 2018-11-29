22593009839df42b4e29fe05c4de996acfc77a93
Kevin Ma <kbma.kevin@gmail.com>
2018-11-28 22:58:59 -0500

    Added launcher activity for selecting which exercise to demo

---
+++ app/src/main/java/josephharrisonlimkevinma/comp304lab6/MainActivity.kt
@@ -0,0 +1,13 @@
+package josephharrisonlimkevinma.comp304lab6
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import comp304lab6.comp304lab6.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
