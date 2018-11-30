134f5af582c9ed62278365e803aa6f4d353d6efa
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-11-30 12:24:41 -0500

    fix broken code

--- app/src/main/java/josephharrisonlimkevinma/comp304lab6/ex1/TransitActivity.kt
+++ app/src/main/java/josephharrisonlimkevinma/comp304lab6/ex1/TransitActivity.kt
@@ -3,6 +3,7 @@ package josephharrisonlimkevinma.comp304lab6.ex1
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.JsonReader
 import android.util.Log
 import android.view.View
 import android.widget.Toast
@@ -15,9 +16,6 @@ import java.io.IOException
 import java.net.URL
 
 class TransitActivity : AppCompatActivity() {
-    companion object {
-        private const val API_URL: String = "https://maps.googleapis.com/maps/api/directions/json"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,22 +27,27 @@ class TransitActivity : AppCompatActivity() {
     fun onGetInfoClick(view: View?) {
         val start = txtStartPoint.text.toString()
         val dest = txtEndPoint.text.toString()
-        val reqStr = "${TransitActivity.API_URL}?origin=${Uri.encode(start)}&destination=${Uri.encode(dest)}&key${R.string.google_maps_key}"
+        val url = "https://maps.googleapis.com/maps/api/directions/json"
+        val reqStr = "$url?origin=${Uri.encode(start)}&destination=${Uri.encode(dest)}&key=${resources.getString(R.string.google_maps_key)}"
         transitRequest(reqStr)
     }
 
     private fun transitRequest(req: String) {
-
         doAsync {
-            Log.d("TRANSIT", "making request")
-            val reqTxt = URL(req).readText()
+            Log.d("QWERTY", "making request")
+            var reqTxt = ""
+            try {
+                reqTxt = URL(req).readText()
+            } catch (e: Throwable) {
+                Log.d("QWERTY", e.toString())
+            }
             //val reqTxt = URL(req).openStream().bufferedReader().readText()
-            Log.d("TRANSIT", "Request finished")
+            Log.d("QWERTY", "Request finished")
 
             uiThread {
-                Log.d("TRANSIT", "Parsing request")
+                Log.d("QWERTY", "Parsing request")
                 val parsedStr = parseRequest(reqTxt)
-                Log.d("TRANSIT", "Setting text directions")
+                Log.d("QWERTY", "Setting text directions")
                 txtDirections.text = parsedStr
             }
         }
@@ -53,14 +56,14 @@ class TransitActivity : AppCompatActivity() {
     private fun parseRequest(reqTxt: String): String {
         val stepStr = StringBuilder()
         try {
-            ObjectMapper().readTree(reqTxt).map { node ->
-                val route = node["routes"][0]
-                val leg = route["legs"][0]
-                val steps = leg["steps"]
-                for (step in steps) {
-                    stepStr.appendln(step["html_instructions"].asText())
-                }
+            val json = ObjectMapper().readTree(reqTxt)
+            val route = json["routes"][0]
+            val leg = route["legs"][0]
+            val steps = leg["steps"]
+            for (step in steps) {
+                stepStr.appendln(step["html_instructions"].asText())
             }
+
         }
         catch (e: Throwable) {
             Log.e("PARSE_ERR", e.toString())
