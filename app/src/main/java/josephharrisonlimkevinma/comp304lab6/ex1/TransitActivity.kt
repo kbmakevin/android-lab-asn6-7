c432d6aa7e42a3f44959983b0e9d1c246d99971d
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-11-30 11:47:19 -0500

    add broken code

--- app/src/main/java/josephharrisonlimkevinma/comp304lab6/ex1/TransitActivity.kt
+++ app/src/main/java/josephharrisonlimkevinma/comp304lab6/ex1/TransitActivity.kt
@@ -3,11 +3,15 @@ package josephharrisonlimkevinma.comp304lab6.ex1
 import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
 import comp304lab6.comp304lab6.R
 import kotlinx.android.synthetic.main.activity_transit.*
-import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.*
+import java.io.IOException
 import java.net.URL
 
 class TransitActivity : AppCompatActivity() {
@@ -26,16 +30,41 @@ class TransitActivity : AppCompatActivity() {
         val start = txtStartPoint.text.toString()
         val dest = txtEndPoint.text.toString()
         val reqStr = "${TransitActivity.API_URL}?origin=${Uri.encode(start)}&destination=${Uri.encode(dest)}&key${R.string.google_maps_key}"
-
+        transitRequest(reqStr)
     }
 
     private fun transitRequest(req: String) {
-        doAsync {
-            var lines = emptyList<String>()
-            URL(req).openStream().use {
-                lines = it.bufferedReader().readLines()
-            }
 
+        doAsync {
+            Log.d("TRANSIT", "making request")
+            val reqTxt = URL(req).readText()
+            //val reqTxt = URL(req).openStream().bufferedReader().readText()
+            Log.d("TRANSIT", "Request finished")
+
+            uiThread {
+                Log.d("TRANSIT", "Parsing request")
+                val parsedStr = parseRequest(reqTxt)
+                Log.d("TRANSIT", "Setting text directions")
+                txtDirections.text = parsedStr
+            }
         }
+    }
+
+    private fun parseRequest(reqTxt: String): String {
+        val stepStr = StringBuilder()
+        try {
+            ObjectMapper().readTree(reqTxt).map { node ->
+                val route = node["routes"][0]
+                val leg = route["legs"][0]
+                val steps = leg["steps"]
+                for (step in steps) {
+                    stepStr.appendln(step["html_instructions"].asText())
+                }
+            }
+        }
+        catch (e: Throwable) {
+            Log.e("PARSE_ERR", e.toString())
+        }
+        return stepStr.toString()
     }
 }
